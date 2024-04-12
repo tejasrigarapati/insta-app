35e005d78ee405e49fe8d8c807b30916be3b0f8b
Swati <jswati603@gmail.com>
2024-04-12 11:08:46 +0530

    login part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Signup from "./Components/Signup"
+import Login from "./Components/Login"
 
-const App=()=>{
+function App(){
+    const[token,setToken]=useState("");
     return(
-        <div>
-            <h1>Hello-World</h1>
+        <div className="App">
+            {/* <Signup setToken={setToken}/> */}
+            <Login setToken={setToken}/>
         </div>
     )
 
